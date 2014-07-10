a98b4b32e1a7dfb5b09e69b55ef5bcf1a7c52ad9
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-07-10 09:02:10 +0200

    Add create-tags command for hasktags

--- haskell-mode.el
+++ haskell-mode.el
@@ -3,3 +3,10 @@
 
 (add-hook 'haskell-mode-hook '(lambda ()
   (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
+
+
+(defun create-haskell-tags (dir-name)
+  "Create tags file in a haskell project"
+  (interactive "DDirectory: ")
+  (eshell-command
+   (format "find %s -type f -name *.hs | hasktags -e STDIN" dir-name)))
