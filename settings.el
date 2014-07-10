7011bcf2d8c1ddd9f63af22ca5d07b71f4f19b42
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-07-10 08:17:45 +0200

    Disable auto save

--- settings.el
+++ settings.el
@@ -85,3 +85,6 @@
 
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+; disable auto save mode
+(setq auto-save-default nil)
