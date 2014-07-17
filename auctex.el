67c842eef0c4fdb8df89b852a31a0df5d8a36192
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-07-17 09:25:55 +0200

    Stopping bugging me about that goddamn master file!

--- auctex.el
+++ auctex.el
@@ -1,6 +1,6 @@
-(load "auctex.el" nil t t)
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
+; (load "auctex.el" nil t t)
+; (setq TeX-auto-save t)
+; (setq TeX-parse-self t)
+; (setq-default TeX-master nil)
 
 ;; (add-to-list 'preview-gs-options '("-dSAFER"))
