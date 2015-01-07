0c3705e738d38f49f3118917346843f63a69350a
Juliusz Gonera <juliusz.gonera@gmail.com>
2015-01-07 13:13:41 +0100

    Add some useful haskell-mode keybindings
    https://trello.com/c/d2I4me67

--- haskell-mode.el
+++ haskell-mode.el
@@ -10,3 +10,12 @@
   (interactive "DDirectory: ")
   (eshell-command
    (format "find %s -type f -name *.hs | hasktags -e STDIN" dir-name)))
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
