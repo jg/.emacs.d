06ae5934c316c30faafd7c425aa5c2a8d6e675a3
Juliusz Gonera <juliusz.gonera@gmail.com>
2015-06-13 08:21:47 +0200

    Update haskell mode config

--- haskell-mode.el
+++ haskell-mode.el
@@ -1,21 +1,22 @@
 (require 'haskell-mode)
 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
-(add-hook 'haskell-mode-hook '(lambda ()
-  (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
+;; (add-hook 'haskell-mode-hook '(lambda ()
+;;   (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
 
 
-(defun create-haskell-tags (dir-name)
-  "Create tags file in a haskell project"
-  (interactive "DDirectory: ")
-  (eshell-command
-   (format "find %s -type f -name *.hs | hasktags -e STDIN" dir-name)))
+;; (defun create-haskell-tags (dir-name)
+;;   "Create tags file in a haskell project"
+;;   (interactive "DDirectory: ")
+;;   (eshell-command
+;;    (format "find %s -type f -name *.hs | hasktags -e STDIN" dir-name)))
 
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+;; (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+;; (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+;; (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+;; (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+;; (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+;; (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+;; (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+;; (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
