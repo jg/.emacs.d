52caf916909fdaa2ed82cb04d28dfa4daaeadf53
Juliusz Gonera <juliusz.gonera@gmail.com>
2015-06-17 13:50:44 +0200

    Enable Ctrl+V
    Refs(JNGx15cY)

--- settings.el
+++ settings.el
@@ -1,3 +1,8 @@
+; enable ctrl+v
+(cua-mode t)
+    (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+    (transient-mark-mode 1)               ;; No region when it is not highlighted
+    (setq cua-keep-region-after-copy t)
 
 ; render images by default
 (auto-image-file-mode)
